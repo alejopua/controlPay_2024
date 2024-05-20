fb3c744338637d7a4830cdbe48fdba461cb05413
alejopua <alejopua@gmail.com>
2024-05-20 04:18:23 -0500

    chore: Update ControlPayPage to improve UI and functionality

--- src/controlPay/pages/ControlPayPage.jsx
+++ src/controlPay/pages/ControlPayPage.jsx
@@ -70,18 +70,27 @@ export const ControlPayPage = () => {
       </header>
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
         <div className="w-full grid gap-4 md:gap-8 ">
-          <Card className="xl:col-span-2" x-chunk="dashboard-01-chunk-4">
-            <CardHeader className="flex flex-row items-center">
+          <Card className="xl:col-span-2">
+            <CardHeader className="flex flex-row items-center justify-between">
               <div className="grid gap-2">
                 <CardTitle>Pagos</CardTitle>
                 <CardDescription>Recent transaction.</CardDescription>
               </div>
-              <Button asChild size="sm" className="ml-auto gap-1">
-                <Link href="#">
-                  Edit
-                  <Pencil className="h-4 w-4" />
-                </Link>
-              </Button>
+              <div className="flex flex-row items-center gap-3">
+                <Button
+                  asChild
+                  size="sm"
+                  className="ml-auto gap-1 cursor-pointer"
+                >
+                  <span>
+                    Edit
+                    <Pencil className="ml-1 h-4 w-4" />
+                  </span>
+                </Button>
+                <span className="text-sm">
+                  To collect <CardTitle>182 USD</CardTitle>
+                </span>
+              </div>
             </CardHeader>
             <CardContent>
               <hr />
