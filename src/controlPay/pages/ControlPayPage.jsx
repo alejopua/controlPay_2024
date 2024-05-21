09229b2af0a9ae91e3d0dac1c9578d1f171e05e4
alejopua <alejopua@gmail.com>
2024-05-21 02:16:02 -0500

    feat: Add MiddlePay component to ControlPayPage

--- src/controlPay/pages/ControlPayPage.jsx
+++ src/controlPay/pages/ControlPayPage.jsx
@@ -23,6 +23,7 @@ import { MdOutlineModeEditOutline } from "react-icons/md";
 import { PaymentsLayout } from "../layout/PaymentsLayout";
 import { StarPay } from "../components/StartPay";
 import PaymentModal from "../components/PaymentModal";
+import { MiddlePay } from "../components/MiddlePay";
 
 const DEFAULT_STATE = {
   payments: [
@@ -145,32 +146,19 @@ export const ControlPayPage = () => {
                   addPrevPay={() => {}}
                   addNextPay={() => {}}
                   PaymentModalComponent={PaymentModal}
-                  paymentModalProps={{ id: "oeee" }}
+                  paymentModalProps={{ id: "oeee S" }}
                 />
 
                 {/* pay middle */}
-                <div className="h-36 relative w-fit flex flex-row items-start ">
-                  <div className="h-fit relative flex opacity-100 transition-opacity duration-300 hover:opacity-100 top-8">
-                    <div className="h-0.5 w-12 bg-gray-200 rounded"></div>
-                  </div>
-                  <div className="relative flex flex-col justify-center items-center top-0">
-                    <button className="relative size-16 flex items-center justify-center bg-gray-200 border-black border-2  rounded-full group">
-                      <MdOutlineModeEditOutline className="size-5 text-black opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
-                    </button>
-
-                    <div className="absolute w-max text-xs text-center md:text-base top-16">
-                      <h1>{"Anticipo"}</h1>
-                      <h2>{"54,6 USD (30%)"}</h2>
-                      <h3>{"22 Ene, 2022"}</h3>
-                    </div>
-                  </div>
-                  <div className="h-fit relative flex opacity-100 transition-opacity duration-300 hover:opacity-100 top-8">
-                    <div className="h-0.5 w-16 bg-gray-200 rounded"></div>
-                    <button className="absolute right-0 top-1/2 transform -translate-y-1/2 w-6 h-6 bg-gray-200 text-xs text-center rounded-full flex items-center justify-center">
-                      +
-                    </button>
-                  </div>
-                </div>
+                <MiddlePay
+                  title={"pago 1"}
+                  amount={91.0}
+                  percentage={100}
+                  date={"2022-01-23"}
+                  addNextPay={() => {}}
+                  PaymentModalComponent={PaymentModal}
+                  paymentModalProps={{ id: "oeee M" }}
+                />
               </PaymentsLayout>
             </CardContent>
           </Card>
