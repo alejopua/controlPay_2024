5099c20e8a671180cbe011cdd3a7b64c4891826d
alejopua <alejopua@gmail.com>
2024-05-21 00:53:03 -0500

    feat: Add PaymentsLayout component for ControlPayPage

---
+++ src/controlPay/layout/PaymentsLayout.jsx
@@ -0,0 +1,7 @@
+export const PaymentsLayout = ({ children }) => {
+  return (
+    <div className="w-full py-14 pb-6">
+      <div className="flex items-center justify-start">{children}</div>
+    </div>
+  );
+};
