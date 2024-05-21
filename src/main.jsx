aca52a4a450059bd798f55fc938b03ddd9d62a5f
alejopua <alejopua@gmail.com>
2024-05-21 18:14:15 -0500

    config: init store with redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ControlPayApp } from "./ControlPayApp";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import { ControlPayApp } from "./ControlPayApp";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ControlPayApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ControlPayApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
