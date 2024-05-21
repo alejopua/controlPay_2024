19a4aa1c698c500ff2dd8e99d30fb4a44cb250a7
alejopua <alejopua@gmail.com>
2024-05-21 17:20:46 -0500

    refactor: Improve StartPay component in ControlPayPage

--- src/controlPay/components/StartPay.jsx
+++ src/controlPay/components/StartPay.jsx
@@ -7,17 +7,22 @@ export const StarPay = ({
   addNextPay,
   PaymentModalComponent,
   paymentModalProps,
+  statusPay,
 }) => {
   return (
     <div className="h-36 relative w-fit flex flex-row items-start ">
-      <div className="h-fit relative flex opacity-0 transition-opacity duration-300 hover:opacity-100 top-8">
+      <div className="h-fit relative top-8">
         <button
           onClick={addPrevPay}
-          className="absolute top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-6 h-6 bg-gray-200 text-xs text-center rounded-full flex items-center justify-center"
+          className={`${
+            statusPay === "pending"
+              ? "opacity-0 transition-opacity duration-300 hover:opacity-100"
+              : "hidden"
+          } size-8 absolute right-1 top-1/2 transform -translate-y-1/2 bg-gray-200 text-xs text-center rounded-full`}
         >
           +
         </button>
-        <div className="h-0.5 w-8 bg-gray-200 rounded"></div>
+        <div className="h-0.5 w-12 bg-white"></div>
       </div>
       <div className="relative flex flex-col justify-center items-center top-0">
         {PaymentModalComponent && (
@@ -34,11 +39,22 @@ export const StarPay = ({
                 : percentage.toFixed(1)
             }%)`}
           </h2>
-          <h3>{date}</h3>
+          {statusPay !== "pending" ? (
+            <span>
+              <p className="text-sm text-green-600">{`Pagado el ${date}`}</p>
+              <p className="text-sm text-green-600">{`con ${statusPay}`}</p>
+            </span>
+          ) : (
+            <p className="text-sm text-gray-500">{date}</p>
+          )}
         </div>
       </div>
       <div className="h-fit relative flex opacity-100 transition-opacity duration-300 hover:opacity-100 top-8">
-        <div className="h-0.5 w-16 bg-gray-200 rounded"></div>
+        <div
+          className={`h-0.5 w-16 ${
+            statusPay === "pending" ? "bg-gray-200" : "bg-green-600"
+          } rounded`}
+        ></div>
         <button
           onClick={addNextPay}
           className="absolute right-0 top-1/2 transform -translate-y-1/2 w-6 h-6 bg-gray-200 text-xs text-center rounded-full flex items-center justify-center"
