792e3e4d3a9dbed6a4988c501f86105a1d4d36a7
alejopua <alejopua@gmail.com>
2024-05-21 01:57:11 -0500

    feat: Add PaymentModal component from Shadcn UI for ControlPayPage

--- src/controlPay/components/PaymentModal.jsx
+++ src/controlPay/components/PaymentModal.jsx
@@ -1,3 +1,83 @@
-export const PaymentModal = () => {
-  return <div>PaymentModal</div>;
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { MdOutlineModeEditOutline } from "react-icons/md";
+import { PiTrashLight } from "react-icons/pi";
+
+const PaymentModal = ({ id }) => {
+  console.log(id);
+  return (
+    <>
+      <Dialog>
+        <DialogTrigger asChild>
+          <button className="relative size-16 flex items-center justify-center bg-gray-200 border-black border-2  rounded-full group">
+            <MdOutlineModeEditOutline className="size-5 text-black opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+          </button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Pagar</DialogTitle>
+            <DialogDescription>Selecciona metodo de pago.</DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="state" className="text-right">
+                Estado
+              </Label>
+              <Select id="state" placeholder="Select a Payment">
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Select a Payment" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Tipo de pago</SelectLabel>
+                    <SelectItem value="cash">Efectivo</SelectItem>
+                    <SelectItem value="card">Tarjeta</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button
+                onClick={() => console.log("Borrado")}
+                variant="outline"
+                size="icon"
+              >
+                <PiTrashLight className="size-6 text-gray-400" />
+              </Button>
+            </DialogClose>
+
+            <DialogClose asChild>
+              <Button onClick={() => console.log("Pago asignado y guardado")}>
+                Guardar
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
 };
+
+export default PaymentModal;
