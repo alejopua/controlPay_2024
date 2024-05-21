e52db4abbfd8a776a209e0a6109233d394561f86
alejopua <alejopua@gmail.com>
2024-05-21 18:04:56 -0500

    refactor: Improve EndPay component UI and add payment status display

--- src/controlPay/components/EndPay.jsx
+++ src/controlPay/components/EndPay.jsx
@@ -6,11 +6,16 @@ export const EndPay = ({
   addNextPay,
   PaymentModalComponent,
   paymentModalProps,
+  statusPay,
 }) => {
   return (
     <div className="h-36 relative w-fit flex flex-row items-start ">
       <div className="h-fit relative flex opacity-100 transition-opacity duration-300 hover:opacity-100 top-8">
-        <div className="h-0.5 w-12 bg-gray-200 rounded"></div>
+        <div
+          className={`h-0.5 w-12 ${
+            statusPay === "pending" ? "bg-gray-200" : "bg-green-600"
+          } rounded`}
+        ></div>
       </div>
       <div className="relative flex flex-col justify-center items-center top-0">
         {PaymentModalComponent && (
@@ -26,14 +31,25 @@ export const EndPay = ({
               ? percentage.toString()
               : percentage.toFixed(1)
           }%) }`}</h2>
-          <h3>{date}</h3>
+          {statusPay !== "pending" ? (
+            <span>
+              <p className="text-sm text-green-600">{`Pagado el ${date}`}</p>
+              <p className="text-sm text-green-600">{`con ${statusPay}`}</p>
+            </span>
+          ) : (
+            <p className="text-sm text-gray-500">{date}</p>
+          )}
         </div>
       </div>
-      <div className="h-fit relative flex opacity-0 transition-opacity duration-300 hover:opacity-100 top-8">
-        <div className="h-0.5 w-16 bg-gray-200 rounded"></div>
+      <div className="h-fit relative flex top-8">
+        <div className="h-0.5 w-16 bg-white"></div>
         <button
           onClick={addNextPay}
-          className="absolute right-0 top-1/2 transform -translate-y-1/2 w-6 h-6 bg-gray-200 text-xs text-center rounded-full flex items-center justify-center"
+          className={`${
+            statusPay === "pending"
+              ? "opacity-0 transition-opacity duration-300 hover:opacity-100"
+              : "hidden"
+          } size-8 absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-200 text-xs text-center rounded-full`}
         >
           +
         </button>
