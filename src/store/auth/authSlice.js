c6c6202691d70a55814617314ef63db37f9e2fa7
alejopua <alejopua@gmail.com>
2024-05-21 23:53:34 -0500

    refactor: Update authentication status to "checking" initially

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  status: "unauthenticated", // "checking" | "authenticated" | "unauthenticated"
+  status: "checking", // "checking" | "authenticated" | "unauthenticated"
   uid: null,
   email: null,
   displayName: null,
