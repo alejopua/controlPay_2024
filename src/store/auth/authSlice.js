45324b8c2d515240e46588e8642bdac5a416ef0f
alejopua <alejopua@gmail.com>
2024-05-21 19:04:58 -0500

    refactor: Update authentication status to "unauthenticated" in authSlice

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  status: "checking", // "checking" | "authenticated" | "unauthenticated"
+  status: "unauthenticated", // "checking" | "authenticated" | "unauthenticated"
   uid: null,
   email: null,
   displayName: null,
