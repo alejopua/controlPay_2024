0740da51cdf95fe674b6280e0830ce4cb9f4dd4e
alejopua <alejopua@gmail.com>
2024-05-22 13:09:58 -0500

    refactor: Update PaymentsLayout to include max width and overflow scroll

--- src/controlPay/layout/PaymentsLayout.jsx
+++ src/controlPay/layout/PaymentsLayout.jsx
@@ -1,6 +1,6 @@
 export const PaymentsLayout = ({ children }) => {
   return (
-    <div className="w-full py-14 pb-6">
+    <div className="w-full max-w-[913px] overflow-scroll py-14 pb-6">
       <div className="flex items-center justify-start">{children}</div>
     </div>
   );
