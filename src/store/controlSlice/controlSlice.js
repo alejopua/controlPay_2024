ee488f0106358e23be78910c671669a4a0f62ecc
alejopua <alejopua@gmail.com>
2024-05-22 14:15:19 -0500

    refactor: Split payment in half and add new payment

--- src/store/controlSlice/controlSlice.js
+++ src/store/controlSlice/controlSlice.js
@@ -15,7 +15,7 @@ export const controlSlice = createSlice({
       },
       {
         id: crypto.randomUUID(),
-        name: "Anticipo",
+        name: "Anticipo 2",
         amount: 182.0,
         percentage: 100,
         status: "pending", // card | cash
@@ -23,15 +23,7 @@ export const controlSlice = createSlice({
       },
       {
         id: crypto.randomUUID(),
-        name: "Anticipo",
-        amount: 182.0,
-        percentage: 100,
-        status: "pending", // card | cash
-        date: formattedDate(),
-      },
-      {
-        id: crypto.randomUUID(),
-        name: "Anticipo",
+        name: "Anticipo 3",
         amount: 182.0,
         percentage: 100,
         status: "pending", // card | cash
@@ -42,6 +34,34 @@ export const controlSlice = createSlice({
     totalAmount: 182.0,
   },
   reducers: {
+    splitPrev: (state, { payload }) => {
+      let paymentIndex = state.payments.findIndex((p) => p.id === payload);
+      let payment = state.payments[paymentIndex];
+
+      while (
+        payment &&
+        payment.status !== "pending" &&
+        paymentIndex < state.payments.length - 1
+      ) {
+        paymentIndex += 1;
+        payment = state.payments[paymentIndex];
+      }
+
+      if (payment && payment.status === "pending") {
+        const newAmount = payment.amount / 2;
+        const newPercentage = payment.percentage / 2;
+        payment.amount = newAmount;
+        payment.percentage = newPercentage;
+
+        const newPayment = addNewPayment(
+          state,
+          newAmount,
+          newPercentage,
+          formattedDate()
+        );
+        state.payments.splice(paymentIndex, 0, newPayment);
+      }
+    },
     splitNext: (state, { payload }) => {
       const paymentIndex = state.payments.findIndex((p) => p.id === payload);
       const payment = state.payments[paymentIndex];
@@ -59,10 +79,12 @@ export const controlSlice = createSlice({
           formattedDate()
         );
         state.payments.splice(paymentIndex + 1, 0, newPayment);
-        console.log(newPayment);
       }
+    },
+    removePayment: (state, { payload }) => {
+      state.payments.filter((payment) => payment.id !== payload.id);
     },
   },
 });
 
-export const { splitNext } = controlSlice.actions;
+export const { splitPrev, splitNext } = controlSlice.actions;
