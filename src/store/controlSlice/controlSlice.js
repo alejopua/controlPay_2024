be1cc8368e7c4ac2e5c93cc5ec6c102f6fc57a0b
alejopua <alejopua@gmail.com>
2024-05-22 07:19:51 -0500

    refactor: Split payment in half and add new payment

--- src/store/controlSlice/controlSlice.js
+++ src/store/controlSlice/controlSlice.js
@@ -1,17 +1,68 @@
 import { createSlice } from "@reduxjs/toolkit";
-
-const initialState = {
-  counter: 10,
-};
+import { addNewPayment, formattedDate } from "@/controlPay/helpers";
 
 export const controlSlice = createSlice({
   name: "control",
-  initialState,
+  initialState: {
+    payments: [
+      {
+        id: crypto.randomUUID(),
+        name: "Anticipo",
+        amount: 182.0,
+        percentage: 100,
+        status: "pending", // card | cash
+        date: formattedDate(),
+      },
+      {
+        id: crypto.randomUUID(),
+        name: "Anticipo",
+        amount: 182.0,
+        percentage: 100,
+        status: "pending", // card | cash
+        date: formattedDate(),
+      },
+      {
+        id: crypto.randomUUID(),
+        name: "Anticipo",
+        amount: 182.0,
+        percentage: 100,
+        status: "pending", // card | cash
+        date: formattedDate(),
+      },
+      {
+        id: crypto.randomUUID(),
+        name: "Anticipo",
+        amount: 182.0,
+        percentage: 100,
+        status: "pending", // card | cash
+        date: formattedDate(),
+      },
+    ],
+    isEditing: false,
+    totalAmount: 182.0,
+  },
   reducers: {
-    increment: (state /* action */) => {
-      state.counter += 1;
+    splitNext: (state, { payload }) => {
+      const paymentIndex = state.payments.findIndex((p) => p.id === payload);
+      const payment = state.payments[paymentIndex];
+
+      if (payment && payment.status === "pending") {
+        const newAmount = payment.amount / 2;
+        const newPercentage = payment.percentage / 2;
+        payment.amount = newAmount;
+        payment.percentage = newPercentage;
+
+        const newPayment = addNewPayment(
+          state,
+          newAmount,
+          newPercentage,
+          formattedDate()
+        );
+        state.payments.splice(paymentIndex + 1, 0, newPayment);
+        console.log(newPayment);
+      }
     },
   },
 });
 
-export const { increment } = controlSlice.actions;
+export const { splitNext } = controlSlice.actions;
