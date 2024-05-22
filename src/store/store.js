96ba2f997cb569e330a045aca5403839cd0f9a39
alejopua <alejopua@gmail.com>
2024-05-22 01:23:50 -0500

    refactor: Add controlSlice to store configuration

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./auth/authSlice";
+import { controlSlice } from "./controlSlice/controlSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
+    control: controlSlice.reducer,
   },
 });
