094600b19ee09297ce7ff1762d451dedadd2d747
alejopua <alejopua@gmail.com>
2024-05-22 07:18:28 -0500

    refactor: Update StartPay component to use data object instead of individual props

--- src/controlPay/components/StartPay.jsx
+++ src/controlPay/components/StartPay.jsx
@@ -1,13 +1,9 @@
 export const StarPay = ({
-  title,
-  amount,
-  percentage,
-  date,
+  data,
   addPrevPay,
   addNextPay,
   PaymentModalComponent,
   paymentModalProps,
-  statusPay,
 }) => {
   return (
     <div className="h-36 relative w-fit flex flex-row items-start ">
@@ -15,7 +11,7 @@ export const StarPay = ({
         <button
           onClick={addPrevPay}
           className={`${
-            statusPay === "pending"
+            data.status === "pending"
               ? "opacity-0 transition-opacity duration-300 hover:opacity-100"
               : "hidden"
           } size-8 absolute right-1 top-1/2 transform -translate-y-1/2 bg-gray-200 text-xs text-center rounded-full`}
@@ -29,34 +25,36 @@ export const StarPay = ({
           <PaymentModalComponent {...paymentModalProps} />
         )}
         <div className="absolute w-max text-xs text-center md:text-base top-16">
-          <h1>{title}</h1>
+          <h1>{data.name}</h1>
           <h2>
             {`${
-              Number.isInteger(amount) ? amount.toString() : amount.toFixed(1)
+              Number.isInteger(data.amount)
+                ? data.amount.toString()
+                : data.amount.toFixed(1)
             } USD (${
-              Number.isInteger(percentage)
-                ? percentage.toString()
-                : percentage.toFixed(1)
+              Number.isInteger(data.percentage)
+                ? data.percentage.toString()
+                : data.percentage.toFixed(1)
             }%)`}
           </h2>
-          {statusPay !== "pending" ? (
+          {data.status !== "pending" ? (
             <span>
-              <p className="text-sm text-green-600">{`Pagado el ${date}`}</p>
-              <p className="text-sm text-green-600">{`con ${statusPay}`}</p>
+              <p className="text-sm text-green-600">{`Pagado el ${data.date}`}</p>
+              <p className="text-sm text-green-600">{`con ${data.status}`}</p>
             </span>
           ) : (
-            <p className="text-sm text-gray-500">{date}</p>
+            <p className="text-sm text-gray-500">{data.date}</p>
           )}
         </div>
       </div>
       <div className="h-fit relative flex opacity-100 transition-opacity duration-300 hover:opacity-100 top-8">
         <div
           className={`h-0.5 w-16 ${
-            statusPay === "pending" ? "bg-gray-200" : "bg-green-600"
+            data.status === "pending" ? "bg-gray-200" : "bg-green-600"
           } rounded`}
         ></div>
         <button
-          onClick={addNextPay}
+          onClick={() => addNextPay(data.id)}
           className="absolute right-0 top-1/2 transform -translate-y-1/2 w-6 h-6 bg-gray-200 text-xs text-center rounded-full flex items-center justify-center"
         >
           +
