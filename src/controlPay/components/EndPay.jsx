34b4ab02f40ebcf3e424d43c193bd9fdc5312441
alejopua <alejopua@gmail.com>
2024-05-23 19:59:07 -0500

    refactor: Update EndPay component to include editing functionality with EditPay

--- src/controlPay/components/EndPay.jsx
+++ src/controlPay/components/EndPay.jsx
@@ -1,9 +1,13 @@
+import { useSelector } from "react-redux";
+import { EditPay } from "./EditPay";
+
 export const EndPay = ({
   data,
   addNextPay,
   PaymentModalComponent,
   paymentModalProps,
 }) => {
+  const { isEditing } = useSelector((state) => state.control);
   return (
     <div className="h-36 relative w-fit flex flex-row items-start ">
       <div className="h-fit relative flex opacity-100 transition-opacity duration-300 hover:opacity-100 top-8">
@@ -19,23 +23,31 @@ export const EndPay = ({
         )}
 
         <div className="absolute w-max text-xs text-center md:text-base top-16">
-          <h1>{data.name}</h1>
-          <h2>{`${
-            Number.isInteger(data.amount)
-              ? data.amount.toString()
-              : data.amount.toFixed(1)
-          } USD (${
-            Number.isInteger(data.percentage)
-              ? data.percentage.toString()
-              : data.percentage.toFixed(1)
-          }%)`}</h2>
-          {data.status !== "pending" ? (
-            <span>
-              <p className="text-sm text-green-600">{`Pagado el ${data.date}`}</p>
-              <p className="text-sm text-green-600">{`con ${data.status}`}</p>
-            </span>
+          {isEditing && data.status === "pending" ? (
+            <EditPay data={data} />
           ) : (
-            <p className="text-sm text-gray-500">{data.date}</p>
+            <>
+              <h1 className="text-lg">{data.name}</h1>
+              <p className="text-sm">
+                {`${
+                  Number.isInteger(data.amount)
+                    ? data.amount.toString()
+                    : data.amount.toFixed(1)
+                } USD (${
+                  Number.isInteger(data.percentage)
+                    ? data.percentage.toString()
+                    : data.percentage.toFixed(1)
+                }%)`}
+              </p>
+              {data.status !== "pending" ? (
+                <span>
+                  <p className="text-sm text-green-600">{`Pagado el ${data.date}`}</p>
+                  <p className="text-sm text-green-600">{`con ${data.status}`}</p>
+                </span>
+              ) : (
+                <p className="text-sm text-gray-500">{data.date}</p>
+              )}
+            </>
           )}
         </div>
       </div>
